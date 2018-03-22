b51de9fc6ac15972cb85034c813c8efb0f3c09ff
canoe <781276284@qq.com>
2018-03-22 23:23:12 +0800

    finish day06

--- Day06/Day06/ViewController.swift
+++ Day06/Day06/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationButton: UIButton!
+    var manager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    @IBAction func locationButtonTouch(_ sender: UIButton) {
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    
+    // MARK: CLLocationDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location!) { (placemark, error)->Void in
+            if (error != nil){
+                self.titleLabel.text = "不能定位！！！" + error!.localizedDescription
+                return
+            }
+            
+            if placemark!.count > 0{
+                self.titleLabel.text = (placemark!.first!.administrativeArea ?? " ") + (placemark!.first!.locality ?? " ")
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.titleLabel.text = "定位失败！！！" + error.localizedDescription
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

299e29dea880035ed66c38f51d31bddf3bc5e524
canoe <781276284@qq.com>
2018-04-16 23:57:03 +0800

    15

--- Day15/Day15/AppDelegate.swift
+++ Day15/Day15/AppDelegate.swift
@@ -12,11 +12,55 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var mask : CALayer?
+    var imageView : UIImageView?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        
+        let imageView = UIImageView(frame: (self.window?.frame)!)
+        imageView.image = UIImage(named: "screen")
+        self.window?.addSubview(imageView)
+        
+        self.mask = CALayer()
+        self.mask?.contents = UIImage(named: "twitter")?.cgImage
+        self.mask?.contentsGravity = kCAGravityResizeAspect
+        self.mask?.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
+        self.mask?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask?.position = CGPoint(x: imageView.frame.size.width/2, y: imageView.frame.size.height/2)
+        imageView.layer.mask = mask
+        
+        self.imageView = imageView
+        
+        animateMask()
+        
+        self.window!.backgroundColor = UIColor(red:0.117, green:0.631, blue:0.949, alpha:1)
+        self.window!.makeKeyAndVisible()
+        UIApplication.shared.isStatusBarHidden = true
         return true
+    }
+    
+    func animateMask() {
+        
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self as? CAAnimationDelegate
+        keyFrameAnimation.duration = 0.6
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
+        let initalBounds = NSValue(cgRect: mask!.bounds)
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 80, height: 64.8))
+        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
+        keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        self.mask!.add(keyFrameAnimation, forKey: "bounds")
+        
+        self.perform(#selector(animationDidStop), with: self, afterDelay: 1.1)
+    }
+    
+    @objc func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        self.imageView!.layer.mask = nil
     }
 
     func applicationWillResignActive(_ application: UIApplication) {

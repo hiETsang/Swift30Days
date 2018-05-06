7fc54bac068f7e021e11f3f521fa99b072a39e67
canoe <781276284@qq.com>
2018-05-06 22:54:52 +0800

    21

---
+++ Day21/Day21/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  Day21
+//
+//  Created by canoe on 2018/5/6.
+//  Copyright © 2018年 canoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var dataArray = [
+        pattern(image: "1", name: "Pattern Building"),
+        pattern(image: "2", name: "Joe Beez"),
+        pattern(image: "3", name: "Car It's car"),
+        pattern(image: "4", name: "Floral Kaleidoscopic"),
+        pattern(image: "5", name: "Sprinkle Pattern"),
+        pattern(image: "6", name: "Palitos de queso"),
+        pattern(image: "7", name: "Ready to Go? Pattern"),
+        pattern(image: "8", name: "Sets Seamless"),
+        ]
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let pattern = self.dataArray[indexPath.row]
+        
+        cell.coolImageView.image = UIImage(named: pattern.image)
+        cell.mainLabel.text = pattern.name
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    //    MARK: editCell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .normal, title: "🗑\nDelete") { (action, index) in
+            self.dataArray.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        delete.backgroundColor = UIColor.gray
+        
+        let share = UITableViewRowAction(style: .normal, title: "🤗\nShare") { (action, index) in
+            
+            let firstActivityItem = self.dataArray[indexPath.row]
+            
+            let activityViewController = UIActivityViewController(activityItems: [firstActivityItem.image as NSString], applicationActivities: nil)
+            
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+        share.backgroundColor = UIColor.red
+        
+        let download = UITableViewRowAction(style: .normal, title: "⬇️\nDownload") { action, index in
+            print("Download button tapped")
+        }
+        download.backgroundColor = UIColor.blue
+        
+        return [download,share,delete]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
